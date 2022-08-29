ab8c9dc7b598ff41ed38022db8679fdf12233fd7
Clifford Mwale <cliffmwale97@gmail.com>
2022-08-29 22:16:39 +0200

    Commit

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.use (cors ());
 app.post('/api/makeDepositRequest', new PaymentProcessing().RequestDeposit)
 
 app.listen(process.env.PORT, () => { 
-    console.log("Payment middleware listengin on port %s ...", process.env.PORT)
+    console.log("Payment middleware listening on port %s ...", process.env.PORT)
 })
